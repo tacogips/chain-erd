91aae7be02845758b3d618b9416c15f1663007fc
tacogips <me@tacogips.me>
2017-10-24 16:19:05 +0900

    clean up

--- grpc/gen/erd.pb.go
+++ grpc/gen/erd.pb.go
@@ -10,11 +10,6 @@ import _ "github.com/gogo/protobuf/gogoproto"
 import _ "github.com/gogo/protobuf/types"
 import _ "github.com/gogo/protobuf/types"
 
-import (
-	context "golang.org/x/net/context"
-	grpc "google.golang.org/grpc"
-)
-
 import io "io"
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -63,153 +58,20 @@ func (m *Size) String() string            { return proto.CompactTextString(m) }
 func (*Size) ProtoMessage()               {}
 func (*Size) Descriptor() ([]byte, []int) { return fileDescriptorErd, []int{1} }
 
-type Activity struct {
-	Id float32 `protobuf:"fixed32,1,opt,name=id,proto3" json:"id,omitempty"`
-}
-
-func (m *Activity) Reset()                    { *m = Activity{} }
-func (m *Activity) String() string            { return proto.CompactTextString(m) }
-func (*Activity) ProtoMessage()               {}
-func (*Activity) Descriptor() ([]byte, []int) { return fileDescriptorErd, []int{2} }
-
-type ShowActivityFilter struct {
-	Number     int32 `protobuf:"varint,1,opt,name=number,proto3" json:"number,omitempty"`
-	SkipNumber int32 `protobuf:"varint,2,opt,name=skip_number,json=skipNumber,proto3" json:"skip_number,omitempty"`
-	Order      Order `protobuf:"varint,3,opt,name=order,proto3,enum=erd.Order" json:"order,omitempty"`
-}
-
-func (m *ShowActivityFilter) Reset()                    { *m = ShowActivityFilter{} }
-func (m *ShowActivityFilter) String() string            { return proto.CompactTextString(m) }
-func (*ShowActivityFilter) ProtoMessage()               {}
-func (*ShowActivityFilter) Descriptor() ([]byte, []int) { return fileDescriptorErd, []int{3} }
-
-type ObjectID struct {
-	Type string `protobuf:"bytes,1,opt,name=type,proto3" json:"type,omitempty"`
-	Val  string `protobuf:"bytes,2,opt,name=val,proto3" json:"val,omitempty"`
-}
-
-func (m *ObjectID) Reset()                    { *m = ObjectID{} }
-func (m *ObjectID) String() string            { return proto.CompactTextString(m) }
-func (*ObjectID) ProtoMessage()               {}
-func (*ObjectID) Descriptor() ([]byte, []int) { return fileDescriptorErd, []int{4} }
-
 type Empty struct {
 }
 
 func (m *Empty) Reset()                    { *m = Empty{} }
 func (m *Empty) String() string            { return proto.CompactTextString(m) }
 func (*Empty) ProtoMessage()               {}
-func (*Empty) Descriptor() ([]byte, []int) { return fileDescriptorErd, []int{5} }
+func (*Empty) Descriptor() ([]byte, []int) { return fileDescriptorErd, []int{2} }
 
 func init() {
 	proto.RegisterType((*Coordinate)(nil), "erd.Coordinate")
 	proto.RegisterType((*Size)(nil), "erd.Size")
-	proto.RegisterType((*Activity)(nil), "erd.Activity")
-	proto.RegisterType((*ShowActivityFilter)(nil), "erd.showActivityFilter")
-	proto.RegisterType((*ObjectID)(nil), "erd.ObjectID")
 	proto.RegisterType((*Empty)(nil), "erd.Empty")
 	proto.RegisterEnum("erd.Order", Order_name, Order_value)
 }
-
-// Reference imports to suppress errors if they are not otherwise used.
-var _ context.Context
-var _ grpc.ClientConn
-
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
-
-// Client API for ActivityService service
-
-type ActivityServiceClient interface {
-	Find(ctx context.Context, in *Empty, opts ...grpc.CallOption) (ActivityService_FindClient, error)
-}
-
-type activityServiceClient struct {
-	cc *grpc.ClientConn
-}
-
-func NewActivityServiceClient(cc *grpc.ClientConn) ActivityServiceClient {
-	return &activityServiceClient{cc}
-}
-
-func (c *activityServiceClient) Find(ctx context.Context, in *Empty, opts ...grpc.CallOption) (ActivityService_FindClient, error) {
-	stream, err := grpc.NewClientStream(ctx, &_ActivityService_serviceDesc.Streams[0], c.cc, "/erd.ActivityService/find", opts...)
-	if err != nil {
-		return nil, err
-	}
-	x := &activityServiceFindClient{stream}
-	if err := x.ClientStream.SendMsg(in); err != nil {
-		return nil, err
-	}
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	return x, nil
-}
-
-type ActivityService_FindClient interface {
-	Recv() (*Activity, error)
-	grpc.ClientStream
-}
-
-type activityServiceFindClient struct {
-	grpc.ClientStream
-}
-
-func (x *activityServiceFindClient) Recv() (*Activity, error) {
-	m := new(Activity)
-	if err := x.ClientStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
-// Server API for ActivityService service
-
-type ActivityServiceServer interface {
-	Find(*Empty, ActivityService_FindServer) error
-}
-
-func RegisterActivityServiceServer(s *grpc.Server, srv ActivityServiceServer) {
-	s.RegisterService(&_ActivityService_serviceDesc, srv)
-}
-
-func _ActivityService_Find_Handler(srv interface{}, stream grpc.ServerStream) error {
-	m := new(Empty)
-	if err := stream.RecvMsg(m); err != nil {
-		return err
-	}
-	return srv.(ActivityServiceServer).Find(m, &activityServiceFindServer{stream})
-}
-
-type ActivityService_FindServer interface {
-	Send(*Activity) error
-	grpc.ServerStream
-}
-
-type activityServiceFindServer struct {
-	grpc.ServerStream
-}
-
-func (x *activityServiceFindServer) Send(m *Activity) error {
-	return x.ServerStream.SendMsg(m)
-}
-
-var _ActivityService_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "erd.ActivityService",
-	HandlerType: (*ActivityServiceServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams: []grpc.StreamDesc{
-		{
-			StreamName:    "find",
-			Handler:       _ActivityService_Find_Handler,
-			ServerStreams: true,
-		},
-	},
-	Metadata: "erd.proto",
-}
-
 func (m *Coordinate) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
@@ -262,92 +124,6 @@ func (m *Size) MarshalTo(dAtA []byte) (int, error) {
 		dAtA[i] = 0x15
 		i++
 		i = encodeFixed32Erd(dAtA, i, uint32(math.Float32bits(float32(m.Height))))
-	}
-	return i, nil
-}
-
-func (m *Activity) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalTo(dAtA)
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *Activity) MarshalTo(dAtA []byte) (int, error) {
-	var i int
-	_ = i
-	var l int
-	_ = l
-	if m.Id != 0 {
-		dAtA[i] = 0xd
-		i++
-		i = encodeFixed32Erd(dAtA, i, uint32(math.Float32bits(float32(m.Id))))
-	}
-	return i, nil
-}
-
-func (m *ShowActivityFilter) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalTo(dAtA)
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *ShowActivityFilter) MarshalTo(dAtA []byte) (int, error) {
-	var i int
-	_ = i
-	var l int
-	_ = l
-	if m.Number != 0 {
-		dAtA[i] = 0x8
-		i++
-		i = encodeVarintErd(dAtA, i, uint64(m.Number))
-	}
-	if m.SkipNumber != 0 {
-		dAtA[i] = 0x10
-		i++
-		i = encodeVarintErd(dAtA, i, uint64(m.SkipNumber))
-	}
-	if m.Order != 0 {
-		dAtA[i] = 0x18
-		i++
-		i = encodeVarintErd(dAtA, i, uint64(m.Order))
-	}
-	return i, nil
-}
-
-func (m *ObjectID) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalTo(dAtA)
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *ObjectID) MarshalTo(dAtA []byte) (int, error) {
-	var i int
-	_ = i
-	var l int
-	_ = l
-	if len(m.Type) > 0 {
-		dAtA[i] = 0xa
-		i++
-		i = encodeVarintErd(dAtA, i, uint64(len(m.Type)))
-		i += copy(dAtA[i:], m.Type)
-	}
-	if len(m.Val) > 0 {
-		dAtA[i] = 0x12
-		i++
-		i = encodeVarintErd(dAtA, i, uint64(len(m.Val)))
-		i += copy(dAtA[i:], m.Val)
 	}
 	return i, nil
 }
@@ -417,44 +193,6 @@ func (m *Size) Size() (n int) {
 	}
 	if m.Height != 0 {
 		n += 5
-	}
-	return n
-}
-
-func (m *Activity) Size() (n int) {
-	var l int
-	_ = l
-	if m.Id != 0 {
-		n += 5
-	}
-	return n
-}
-
-func (m *ShowActivityFilter) Size() (n int) {
-	var l int
-	_ = l
-	if m.Number != 0 {
-		n += 1 + sovErd(uint64(m.Number))
-	}
-	if m.SkipNumber != 0 {
-		n += 1 + sovErd(uint64(m.SkipNumber))
-	}
-	if m.Order != 0 {
-		n += 1 + sovErd(uint64(m.Order))
-	}
-	return n
-}
-
-func (m *ObjectID) Size() (n int) {
-	var l int
-	_ = l
-	l = len(m.Type)
-	if l > 0 {
-		n += 1 + l + sovErd(uint64(l))
-	}
-	l = len(m.Val)
-	if l > 0 {
-		n += 1 + l + sovErd(uint64(l))
 	}
 	return n
 }
@@ -634,285 +372,6 @@ func (m *Size) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
-func (m *Activity) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowErd
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= (uint64(b) & 0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: Activity: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: Activity: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 5 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Id", wireType)
-			}
-			var v uint32
-			if (iNdEx + 4) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += 4
-			v = uint32(dAtA[iNdEx-4])
-			v |= uint32(dAtA[iNdEx-3]) << 8
-			v |= uint32(dAtA[iNdEx-2]) << 16
-			v |= uint32(dAtA[iNdEx-1]) << 24
-			m.Id = float32(math.Float32frombits(v))
-		default:
-			iNdEx = preIndex
-			skippy, err := skipErd(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if skippy < 0 {
-				return ErrInvalidLengthErd
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *ShowActivityFilter) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowErd
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= (uint64(b) & 0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: showActivityFilter: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: showActivityFilter: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Number", wireType)
-			}
-			m.Number = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowErd
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.Number |= (int32(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-		case 2:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field SkipNumber", wireType)
-			}
-			m.SkipNumber = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowErd
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.SkipNumber |= (int32(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-		case 3:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Order", wireType)
-			}
-			m.Order = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowErd
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.Order |= (Order(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-		default:
-			iNdEx = preIndex
-			skippy, err := skipErd(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if skippy < 0 {
-				return ErrInvalidLengthErd
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *ObjectID) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowErd
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= (uint64(b) & 0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: ObjectID: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: ObjectID: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Type", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowErd
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= (uint64(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthErd
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Type = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Val", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowErd
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= (uint64(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthErd
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Val = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipErd(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if skippy < 0 {
-				return ErrInvalidLengthErd
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
 func (m *Empty) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
@@ -1071,29 +530,20 @@ var (
 func init() { proto.RegisterFile("erd.proto", fileDescriptorErd) }
 
 var fileDescriptorErd = []byte{
-	// 379 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x91, 0xcd, 0x6e, 0xda, 0x40,
-	0x14, 0x85, 0x19, 0xff, 0xf0, 0x73, 0xdb, 0x52, 0x34, 0x6a, 0x2b, 0xe4, 0x85, 0x41, 0xee, 0x06,
-	0x55, 0x2a, 0x20, 0x5a, 0x65, 0x4f, 0x80, 0x48, 0xd9, 0x04, 0xc9, 0xde, 0x65, 0x13, 0xd9, 0x9e,
-	0xc1, 0x9e, 0x04, 0x7b, 0xac, 0x61, 0x0c, 0x38, 0x4f, 0xc8, 0x32, 0x8f, 0x90, 0xf0, 0x24, 0x91,
-	0xc7, 0x46, 0x91, 0xb2, 0x3b, 0xe7, 0x7e, 0xe7, 0x1e, 0x7b, 0x66, 0xa0, 0x43, 0x05, 0x19, 0x67,
-	0x82, 0x4b, 0x8e, 0x75, 0x2a, 0x88, 0xf5, 0x37, 0x62, 0x32, 0xce, 0x83, 0x71, 0xc8, 0x93, 0x49,
-	0xc4, 0x23, 0x3e, 0x51, 0x2c, 0xc8, 0x37, 0xca, 0x29, 0xa3, 0x54, 0xb5, 0x63, 0xd9, 0x11, 0xe7,
-	0xd1, 0x96, 0x7e, 0xa4, 0x48, 0x2e, 0x7c, 0xc9, 0x78, 0x5a, 0xf3, 0xc1, 0x67, 0x2e, 0x59, 0x42,
-	0x77, 0xd2, 0x4f, 0xb2, 0x2a, 0xe0, 0x8c, 0x00, 0x16, 0x9c, 0x0b, 0xc2, 0x52, 0x5f, 0x52, 0xfc,
-	0x15, 0xd0, 0xb1, 0x8f, 0x86, 0x68, 0xa4, 0xb9, 0xe8, 0x58, 0xba, 0xa2, 0xaf, 0x55, 0xae, 0x70,
-	0xfe, 0x83, 0xe1, 0xb1, 0x67, 0x8a, 0x7f, 0x80, 0x79, 0x60, 0x44, 0xc6, 0x75, 0xae, 0x32, 0xf8,
-	0x17, 0x34, 0x63, 0xca, 0xa2, 0x58, 0xd6, 0x0b, 0xb5, 0x73, 0x2c, 0x68, 0xcf, 0x43, 0xc9, 0xf6,
-	0x4c, 0x16, 0xb8, 0x0b, 0x1a, 0x23, 0xf5, 0x9a, 0xc6, 0x88, 0xc3, 0x01, 0xef, 0x62, 0x7e, 0xb8,
-	0xf0, 0x1b, 0xb6, 0x95, 0x54, 0x94, 0x4d, 0x69, 0x9e, 0x04, 0x54, 0xa8, 0xa4, 0xe9, 0xd6, 0x0e,
-	0x0f, 0xe0, 0xcb, 0xee, 0x89, 0x65, 0x0f, 0x35, 0xd4, 0x14, 0x84, 0x72, 0x74, 0x57, 0x05, 0x86,
-	0x60, 0x72, 0x41, 0xa8, 0xe8, 0xeb, 0x43, 0x34, 0xea, 0xce, 0x60, 0x5c, 0x5e, 0xed, 0xba, 0x9c,
-	0xb8, 0x15, 0x70, 0xa6, 0xd0, 0x5e, 0x07, 0x8f, 0x34, 0x94, 0xb7, 0x4b, 0x8c, 0xc1, 0x90, 0x45,
-	0x46, 0xd5, 0x47, 0x3a, 0xae, 0xd2, 0xb8, 0x07, 0xfa, 0xde, 0xdf, 0xaa, 0xea, 0x8e, 0x5b, 0x4a,
-	0xa7, 0x05, 0xe6, 0x2a, 0xc9, 0x64, 0xf1, 0xc7, 0x02, 0x53, 0x55, 0xe1, 0x16, 0xe8, 0x73, 0x6f,
-	0xd1, 0x6b, 0xe0, 0x36, 0x18, 0xcb, 0x95, 0xb7, 0xe8, 0xa1, 0xd9, 0x15, 0x7c, 0xbf, 0x9c, 0xc1,
-	0xa3, 0x62, 0xcf, 0x42, 0x8a, 0x7f, 0x83, 0xb1, 0x61, 0x29, 0xc1, 0xd5, 0x4f, 0xa8, 0x0a, 0xeb,
-	0x9b, 0xd2, 0x97, 0xe4, 0x14, 0x5d, 0xff, 0x3c, 0xbd, 0xd9, 0x8d, 0xd3, 0xd9, 0x46, 0x2f, 0x67,
-	0x1b, 0xbd, 0x9e, 0x6d, 0x74, 0xaf, 0x47, 0x34, 0x0d, 0x9a, 0xea, 0x65, 0xfe, 0xbd, 0x07, 0x00,
-	0x00, 0xff, 0xff, 0x9b, 0x41, 0xa9, 0x35, 0x1b, 0x02, 0x00, 0x00,
+	// 237 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0xce, 0xc1, 0x4a, 0xc3, 0x40,
+	0x10, 0x06, 0xe0, 0x6e, 0xd3, 0xb4, 0x3a, 0x78, 0x28, 0x8b, 0x4a, 0xc9, 0x61, 0x95, 0x9c, 0x8a,
+	0x60, 0x7b, 0xd0, 0x17, 0xd0, 0xd8, 0xb3, 0x60, 0x6e, 0xde, 0x12, 0x77, 0xdc, 0x2c, 0x98, 0x4c,
+	0x58, 0x27, 0xd8, 0xf8, 0x84, 0x39, 0xfa, 0x08, 0x9a, 0x27, 0x11, 0x37, 0x01, 0xa1, 0xb7, 0xf9,
+	0xf8, 0xff, 0x1f, 0x06, 0x8e, 0xd1, 0xe9, 0x4d, 0xed, 0x88, 0x49, 0x06, 0xe8, 0x74, 0x74, 0x6d,
+	0x2c, 0x17, 0x4d, 0xbe, 0x79, 0xa1, 0x72, 0x6b, 0xc8, 0xd0, 0xd6, 0x67, 0x79, 0xf3, 0xea, 0xe5,
+	0xe1, 0xaf, 0x61, 0x13, 0x29, 0x43, 0x64, 0xde, 0xf0, 0xbf, 0xa5, 0x1b, 0x97, 0xb1, 0xa5, 0x6a,
+	0xcc, 0x2f, 0x0e, 0x73, 0xb6, 0x25, 0xbe, 0x73, 0x56, 0xd6, 0x43, 0x21, 0x5e, 0x03, 0x24, 0x44,
+	0x4e, 0xdb, 0x2a, 0x63, 0x94, 0x27, 0x20, 0xf6, 0x2b, 0x71, 0x29, 0xd6, 0xd3, 0x27, 0xb1, 0xff,
+	0x53, 0xbb, 0x9a, 0x0e, 0x6a, 0xe3, 0x5b, 0x98, 0xa5, 0xf6, 0x13, 0xe5, 0x29, 0x84, 0x1f, 0x56,
+	0x73, 0x31, 0xf6, 0x06, 0xc8, 0x73, 0x98, 0x17, 0x68, 0x4d, 0xc1, 0xe3, 0x60, 0x54, 0xbc, 0x80,
+	0x70, 0x57, 0xd6, 0xdc, 0x5e, 0x45, 0x10, 0x3e, 0x3a, 0x8d, 0x4e, 0x2e, 0x20, 0xb8, 0x4b, 0x93,
+	0xe5, 0x44, 0x1e, 0xc1, 0xec, 0x61, 0x97, 0x26, 0x4b, 0x71, 0x7f, 0xd6, 0xfd, 0xa8, 0x49, 0xd7,
+	0x2b, 0xf1, 0xd5, 0x2b, 0xf1, 0xdd, 0x2b, 0xf1, 0x1c, 0x18, 0xac, 0xf2, 0xb9, 0x7f, 0xf1, 0xe6,
+	0x37, 0x00, 0x00, 0xff, 0xff, 0xd3, 0xb8, 0xc2, 0x19, 0x24, 0x01, 0x00, 0x00,
 }
