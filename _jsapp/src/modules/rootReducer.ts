9755222e15f313d071f1d009872d051fde1dc845
tacogips <me@tacogips.me>
2017-11-09 15:29:04 +0900

    reducer

--- _jsapp/src/modules/rootReducer.ts
+++ _jsapp/src/modules/rootReducer.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
-import {articleReducer as articles, ArticleState } from './articles'
+import { articleReducer as articles, ArticleState } from './articles'
 
 import { Entity, Rel } from 'grpc/erd_pb'
 
-export interface RootState{
-	articles? : ArticleState
-	entities:Map<string,Entity>
+export interface RootState {
+    articles?: ArticleState
+    entities: Map<string, Entity>
+    rels: Map<string, Rel>
 }
+
 
 export const rootReducer = combineReducers<RootState>({
     articles,
